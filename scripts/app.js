2b3a1a13640ebca90214294355ff009aa7452db9
ankur <atrivedi@xebia.com>
2015-05-21 18:21:30 +0530

    Initial Commit

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTrainingApp
+ * @description
+ * # angularTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTrainingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/test', {
+        templateUrl: 'views/testView.html',
+        controller: 'TestcontrollerCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
